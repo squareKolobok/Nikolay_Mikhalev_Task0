43bcb6c44995e61fc2e2166b28225e799a7f8690
squareKolobok <kil7000@yandex.ru>
2016-11-07 21:40:59 +0300

    begin

---
+++ Mikhalev_Nikolay_Task1/Mikhalev_Nikolay_Task1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Mikhalev_Nikolay_Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string continWork;
+            do
+            {
+                Console.Write("Выберете график (а - к) \n");
+                string NumGraph = Console.ReadLine();
+                double coordX, coordY;
+                while (true)
+                {
+                    Console.Write("Введите X и Y \n");
+                    if (double.TryParse(Console.ReadLine(), out coordX) && double.TryParse(Console.ReadLine(), out coordY))
+                        break;
+                    else
+                        Console.Write("данные введены не правильно, повторите ввод \n");
+                }
+                switch (NumGraph)
+                {
+                    case "а":
+                        if ((coordX * coordX + coordY * coordY <= 1))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "б":
+                        if ((coordX * coordX + coordY * coordY <= 1) && (coordX * coordX + coordY * coordY >= 0.5))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "в":
+                        if (Math.Abs(coordX) <= 1 && Math.Abs(coordY) <= 1)
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "г":
+                        if ((Math.Abs(coordX) - 1 <= coordY) && (1 - Math.Abs(coordX) >= coordY))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "д":
+                        if ((Math.Abs(2 * coordX) - 1 <= coordY) && (1 - Math.Abs(2 * coordX) >= coordY))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "е":
+                        if ((coordX < 0 && (Math.Abs(coordY) - 2 <= coordX)) || (coordX >= 0 && coordX * coordX + coordY * coordY <= 1))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "ж":
+                        if ((coordY >= -1) && (2 - Math.Abs(2 * coordX) >= coordY))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "з":
+                        if ((Math.Abs(coordX) <= 1 && Math.Abs(coordY + 0.5) <= 1.5) && (Math.Abs(coordX) <= coordY))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "и":
+                        if ((coordY >= 0 && coordX <= -coordY && coordX >= (coordY - 3) / 2) ||
+                            (coordY <= 0 && coordX >= (coordY - 3) / 2 && coordY >= (coordX - 1) / 3))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    case "к":
+                        if ((coordY >= 1) || (Math.Abs(coordX) <= coordY))
+                            WriteResult(coordX, coordY, NumGraph, "");
+                        else
+                            WriteResult(coordX, coordY, NumGraph, "не ");
+                        break;
+                    default:
+                        Console.Write("данные введены неправильно  \n");
+                        break;
+                }
+                Console.Write("Продолжить работу? (y / n) \n");
+                continWork = Console.ReadLine();
+            } while (continWork == "y" || continWork == "н");
+        }
+
+        static void WriteResult(double X, double Y, string figure, string negativ)
+        {
+            Console.Write("Точка (" + X + ";" + Y + ") " + negativ + "принадлежит фигуре " + figure + " \n");
+        }
+    }
+}
---
+++ Mikhalev_Nikolay_Task1/Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double h;
+            string continWork;
+            do
+            {
+                while (true)
+                {
+                    Console.Write("Введите действительное число h\nh=");
+                    if (double.TryParse(Console.ReadLine(), out h))
+                        break;
+                    else
+                        Console.Write("данные введены не правильно, повторите ввод \n");
+                }
+                double a = Math.Sqrt((Math.Abs(Math.Sin(8 * h)) + 17) / Math.Pow((1 - Math.Sin(4 * h) * Math.Cos(h * h + 18)), 2));
+                double b = 1 - Math.Sqrt(3 / (3 + Math.Abs(Math.Tan(a * h * h) - Math.Sin(a * h))));
+                double c = a * h * h * Math.Sin(b * h) + b * Math.Pow(h, 3) * Math.Cos(a * h);
+                double d = b * b - 4 * a * c;
+                Console.Write("a=" + Math.Round(a, 5) + " \n");
+                Console.Write("b=" + Math.Round(b, 5) + " \n");
+                Console.Write("c=" + Math.Round(c, 5) + " \n");
+                Console.Write("Уравнение имеет вид: " + Math.Round(a, 5) + "*x^2+"
+                                                      + Math.Round(b, 5) + "*x+"
+                                                      + Math.Round(c, 5) + "=0 \n");
+                Console.Write("Дискриминант D=" + Math.Round(d, 5) + " \n");
+                if (d < 0)
+                    Console.Write("Уравнение не имеет действительных корней \n");
+                else if (d > 0)
+                {
+                    double x1 = (-b - Math.Sqrt(d)) / (2 * a);
+                    double x2 = (-b + Math.Sqrt(d)) / (2 * a);
+                    Console.Write("Уравнение имеет два действительных корня \nx1="
+                        + Math.Round(x1, 5) + "\nx2=" + Math.Round(x2, 5) + "\n");
+                }
+                else
+                {
+                    double x = -b / (2 * a);
+                    Console.Write("Уравнение имеет один действительный корень x=" + x + "\n");
+                }
+                Console.Write("Желаете повторить? (y / n) \n");
+                continWork = Console.ReadLine();
+            } while (continWork == "y" || continWork == "н");
+        }
+    }
+}
