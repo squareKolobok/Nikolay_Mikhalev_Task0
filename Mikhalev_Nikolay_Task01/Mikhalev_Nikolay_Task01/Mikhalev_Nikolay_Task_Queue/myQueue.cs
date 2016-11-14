a4c2ffad9e474c8ebe9b14830d36109b737a0115
Nikolay Piskarev <Nikolay_Piskarev@epam.com>
2016-11-14 10:03:14 +0400

    Стек и очередь приняты. Переделывать не нужно.

--- Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task_Queue/myQueue.cs
+++ Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task_Queue/myQueue.cs
@@ -2,10 +2,10 @@
 {
     public class myQueue<T>
     {
-        private T data = default(T);//хранимое значение
-        private static int LengthStack = 0;//длина очереди
-        private static myQueue<T> endLink = null;//последний элемент очереди 
-        private myQueue<T> prev = null;//ссылка предыдущий элимент
+        private T data = default(T);//хранимое значение //todo по умолчанию всегда будет присваиваться значение по умолчанию, поэтому достаточно просто "...data;"
+        private static int LengthStack = 0;//длина очереди//todo пока не проходили, но лучше сделать это поле не статическим.
+        private static myQueue<T> endLink = null;//последний элемент очереди //todo пока не проходили, но лучше сделать это поле не статическим.
+        private myQueue<T> prev = null;//ссылка предыдущий элимент//todo судя по сигнатуре, ссылка-то на какой-то другой стек, а не на элемент стека.  
 
         //конструкторы стека
         public myQueue()
@@ -51,9 +51,9 @@
             return default(T);
         }
 
-        public bool Empty()
+        public bool Empty()//todo лучше переименовать в IsEmpty, поскольку метод по сути своей флаг
         {
-            return LengthStack == 0 ? true : false;
+            return LengthStack == 0 ? true : false;//todo return LengthStack == 0 делает тоже самое, что и return LengthStack == 0 ? true : false
         }
     }
 }
--- Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task_Stak/myStack.cs
+++ Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task01/Mikhalev_Nikolay_Task_Stak/myStack.cs
@@ -2,9 +2,9 @@
 {
     public class myStack<T>
     {
-        private T data = default(T);//хранимое значение
+        private T data = default(T);//хранимое значение//todo аналогично очереди
         private static int LengthStack = 0;//длина стека
-        private myStack<T> prev = null;//ссылка предыдущий элимент
+        private myStack<T> prev = null;//ссылка предыдущий элимент//todo это не ссылка на предыдущий элемент, а на какой-то стек
 
         //конструктор стека
         public myStack()
@@ -18,7 +18,7 @@
         public void Push(T param)
         {
             LengthStack++;
-            prev = new myStack<T>(param, prev);
+            prev = new myStack<T>(param, prev);//todo здесь ты создаешь новый объект в памяти, так что такая реализация точно не может быть корректной применительно к "ссылка на предыдущий элемент"
         }
 
         public T Pop()
@@ -44,7 +44,7 @@
 
         public bool Empty()
         {
-            return LengthStack == 0 ? true : false;
+            return LengthStack == 0 ? true : false;//todo аналогично очереди
         }
     }
 }
